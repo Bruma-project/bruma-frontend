2bb374196642302025da45c0218ccf1bb86d6fed
Reta <r.range755@gmail.com>
2023-02-15 10:38:04 -0300

    export login and error404page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,4 @@
 export * from "./character-sheet-creator/Character-sheet-creator.page";
 export * from "./home/Home.page";
+export * from "./login/Login.page";
+export * from "./error404/Error404.page";
